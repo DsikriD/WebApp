7f49be953aef695b79bdde94c38e712ed75ccd75
DsikriD <sikrierdanila@yandex.ru>
2023-11-21 17:43:54 +0300

    Добавьте файлы проекта.

---
+++ DataLibrary/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataLibrary
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using DataLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Models.ViewModel;
+using System.Diagnostics;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            CategoryVM category = new CategoryVM() {
+                CategoryList = _db.Category
+            };
+
+    
+            return View(category);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
